c0e26ab171adbc4b2841bad54c87c0e4b876deaa
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2026-01-24 16:26:04 +0100

    something done!

--- src/dataset-construction/read_simulation.py
+++ src/dataset-construction/read_simulation.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import logging
 from tqdm.notebook import tqdm
 from typing import Tuple
+import math
+
 
 
 def combine_references(split_dir: Path, output_fasta: Path) -> bool:
@@ -30,19 +32,68 @@ def combine_references(split_dir: Path, output_fasta: Path) -> bool:
         return True
 
 
-def read_simulation(combined_ref, out_dir: Path, N_READS: int, CORES: int) -> Tuple[Path, Path]:
+def run_simulation(split: 'str',
+                   input_dir: Path,
+                   output_dir: Path,
+                   N_READS: int,
+                   BATCH_SIZE: int,
+                   CORES: int) -> None:
     """
     Runs InSilicoSeq to generate paired-end reads
     """
     logging.info(f'[2 / 4] Simulating {N_READS} reads with InSilicoSeq...')
 
-    out_prefix = out_dir / 'simulated'
+    all_genomes = list(input_dir.glob('**/*.fna'))
 
-    cmd = (f'iss generate --genome {combined_ref}'
-           f'--model miseq --n_reads {N_READS}'
-           f'--output {out_prefix} --cpus {CORES}')
+    if not all_genomes:
+        logging.warning(f'No genomes found in {input_dir}')
+        return
 
-    if os.system(cmd) != 0:
-        raise RuntimeError('InSilicoSeq failed to generate reads')
+    num_batches = math.ceil(len(all_genomes) / BATCH_SIZE)
+    reads_per_batch = int(N_READS / num_batches)
 
-    return out_prefix.with_name('simulated_R1.fastq'), out_prefix.with_name('simulated_R2.fastq')
+    logging.info(f'Found {len(all_genomes)} genomes')
+    logging.info(f'Processing in {num_batches} batches of size {BATCH_SIZE} files.')
+    logging.info(f'Generating {reads_per_batch} reads per batch')
+
+    final_r1 = output_dir / 'simulated_R1.fastq'
+    final_r2 = output_dir / 'simulated_R2.fastq'
+
+    if final_r1.exists(): final_r1.unlink()
+    if final_r2.exists(): final_r2.unlink()
+
+    temp_dir = output_dir / 'temp'
+    temp_dir.mkdir(exist_ok=True)
+
+    batch_iterator = get_file_batches(all_genomes, BATCH_SIZE)
+
+    for i, batch_files in enumerate(tqdm(batch_iterator, total=num_batches, desc='Simulating Batches')):
+        batch_ref = temp_dir / 'batch_ref.fasta'
+        merge_fasta_batch(batch_files, batch_ref)
+
+        batch_out_prefix = temp_dir / f'batch_{i}'
+        iss_cmd = (f'iss generate --genomes {batch_ref}'
+                   f'--model miseq --n_reads {reads_per_batch}'
+                   f'--output {batch_out_prefix} --cpus {CORES}'
+                   )
+
+        exit_code = os.system(iss_cmd)
+        if exit_code != 0:
+            logging.warning(f'Batch {i} failed with exit code {exit_code}')
+            continue
+
+        batch_r1 = batch_out_prefix / 'simulated_R1.fastq'
+        batch_r2 = batch_out_prefix / 'simulated_R2.fastq'
+
+        if batch_r1.exists() and batch_r2.exists():
+            append_fastq(batch_r1, final_r1)
+            append_fastq(batch_r2, final_r2)
+
+        for f in temp_dir.glob('*'):
+            f.unlink()
+
+    if temp_dir.exists():
+        temp_dir.rmdir()
+
+    logging.info(f'Simulation completed on {split}')
+    logging.info(f'Outputs: {final_r1},  {final_r2}')
